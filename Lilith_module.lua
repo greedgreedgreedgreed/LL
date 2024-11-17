39aed74003f8843ec2d1464f18171f6431f55984
greedgreedgreed <147218564+greedgreedgreedgreed@users.noreply.github.com>
2024-11-17 16:55:21 -0500

    Update and rename util.lua to Lilith_module.lua

--- Lilith_module.lua
+++ Lilith_module.lua
@@ -1,4 +1,4 @@
-local nurysium_module = {}
+local Lilith_module = {}
 
 local Players = game:GetService("Players")
 
@@ -7,11 +7,11 @@ local Services = {
     game:GetService('SocialService')
 }
 
-function nurysium_module.isAlive(Entity)
+function Lilith_module.isAlive(Entity)
     return Entity.Character and workspace.Alive:FindFirstChild(Entity.Name) and workspace.Alive:FindFirstChild(Entity.Name).Humanoid.Health > 0
 end
 
-function nurysium_module.getBall()
+function Lilith_module.getBall()
     for index, ball in workspace:WaitForChild("Balls"):GetChildren() do
         if ball:IsA("BasePart") and ball:GetAttribute("realBall") then
             return ball
@@ -19,4 +19,12 @@ function nurysium_module.getBall()
     end
 end
 
-return nurysium_module;
+function Lilith_module.getTrainingBall()
+    for index, ball in workspace:WaitForChild("TrainingBalls"):GetChildren() do
+        if ball:IsA("BasePart") and ball:GetAttribute("realBall") then
+            return ball
+        end
+    end
+end
+
+return Lilith_module
